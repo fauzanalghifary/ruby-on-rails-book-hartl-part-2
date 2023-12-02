a2710b98b855003f8a5edfbc8b964398acf92aa7
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-12-02 19:24:32 +0700

    chapter 8 finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new', status: :unprocessable_entity
@@ -13,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
